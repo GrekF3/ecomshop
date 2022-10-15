beec16bdfaaf394f214917f1a45c67d928507ef1
GrekF3 <v.beats@inbox.ru>
2022-10-15 23:50:14 +0300

    Update main, contact,about

---
+++ main/shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from shop import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/',views.contacts, name='contact'),
+]
